21f8863afbbf25969d06d9064681dacc149b9dfc
ChngYrNick <cakedispensers@gmail.com>
2021-04-16 05:06:13 +0300

    Change setup db script

--- application/db/setup.sh
+++ application/db/setup.sh
@@ -1,3 +1,3 @@
 psql -f install.sql -U postgres
-PGPASSWORD=12345 psql -d test -f structure.sql -U serhii
-PGPASSWORD=12345 psql -d test -f data.sql -U serhii
+PGPASSWORD=12345 psql -d test -f structure.sql -U serhii -h localhost
+PGPASSWORD=12345 psql -d test -f data.sql -U serhii -h localhost
